dfc5ece97be5250c76b2cbd2de64a842a66ab3bd
wang-zhong-ping <wang-zhong-ping@163.com>
2012-04-09 15:55:02 +0800

    添加监测器方法，create(List<Map<String,String>>)

--- src/COM/dragonflow/Page/TestAddMoniterPage.java
+++ src/COM/dragonflow/Page/TestAddMoniterPage.java
@@ -9,6 +9,12 @@
  */
 package COM.dragonflow.Page;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import COM.dragonflow.Api.APIMonitor;
 import COM.dragonflow.Api.SSInstanceProperty;
 import COM.dragonflow.SiteViewException.SiteViewException;
@@ -18,18 +24,17 @@ import COM.dragonflow.SiteViewException.SiteViewException;
 
 public class TestAddMoniterPage extends COM.dragonflow.Page.CGI {
 	static COM.dragonflow.Api.APIMonitor apimonitor = new APIMonitor();
+
 	public TestAddMoniterPage() {
 	}
 
 	public void printBody() throws java.lang.Exception {
 		printBodyHeader("Hello");
-		outputStream
-				.println("<p>\n<CENTER><H2>hello</H2></CENTER><P>\n<p>\n"
-						+ "<a href=http://localhost:9999/SiteView>"
-						+ "<input type=\"button\" value=\"hello\" class=\"VerBl8\">\n"
-						+"</a>"
-						+ "</FORM>\n");
-		createMonitor();
+		outputStream.println("<p>\n<CENTER><H2>hello</H2></CENTER><P>\n<p>\n"
+				+ "<a href=http://localhost:9999/SiteView>"
+				+ "<input type=\"button\" value=\"hello\" class=\"VerBl8\">\n"
+				+ "</a>" + "</FORM>\n");
+		// createMonitor();
 	}
 
 	public static void main(String args[]) throws java.io.IOException {
@@ -39,56 +44,75 @@ public class TestAddMoniterPage extends COM.dragonflow.Page.CGI {
 		}
 		ftppage.handleRequest();
 	}
-	public static void createMonitor() throws SiteViewException{
+
+	public static void createMonitor(List<Map<String, String>> list)
+			throws SiteViewException {
 		SSInstanceProperty[] assinstanceproperty = new SSInstanceProperty[10];
-		
-		assinstanceproperty[0] = new SSInstanceProperty("_host", "192.168.0.248");
-	    assinstanceproperty[1] = new SSInstanceProperty("_oidIndex", "0");
-	    assinstanceproperty[2] = new SSInstanceProperty("_timeout", "5");
-	    assinstanceproperty[3] = new SSInstanceProperty("_snmpversion", "v2");
-	    assinstanceproperty[4] = new SSInstanceProperty("_retryDelay", "1");
-	    assinstanceproperty[5] = new SSInstanceProperty("_community", "dragon");
-	    assinstanceproperty[6] = new SSInstanceProperty("_frequency", "600");
-	    assinstanceproperty[7] = new SSInstanceProperty("error-condition0", "default");
-	    assinstanceproperty[8] = new SSInstanceProperty("warning-condition0", "default");
-	    assinstanceproperty[9] = new SSInstanceProperty("good-condition0", "default");
-        
-		COM.dragonflow.Api.SSStringReturnValue ssstringreturnvalue2 = apimonitor.create("SNMPMonitor", "Group0", assinstanceproperty);
-		
-		
-		
-		
-//		
-//		
-//		
-//		
-//		
-//		
-//		
-//		SSInstanceProperty[] assinstanceproperty = new SSInstanceProperty[15];
-//		
-//		assinstanceproperty[0] = new SSInstanceProperty("_nbmonitorVersion", "-1");
-//        assinstanceproperty[1] = new SSInstanceProperty("_nbmonitorVersion", "-1");
-//        assinstanceproperty[2] = new SSInstanceProperty("_community", "dragon");
-//        assinstanceproperty[3] = new SSInstanceProperty("_community", "dragon");
-//        assinstanceproperty[4] = new SSInstanceProperty("_maxRTDataWindow", "24");
-//        assinstanceproperty[5] = new SSInstanceProperty("_timeout", "5");
-//        assinstanceproperty[6] = new SSInstanceProperty("_timeout", "5");
-//        assinstanceproperty[10] = new SSInstanceProperty("_retries", "1");
-//        assinstanceproperty[11] = new SSInstanceProperty("_retries", "1");
-//        assinstanceproperty[12] = new SSInstanceProperty("_port", "161");
-//        assinstanceproperty[13] = new SSInstanceProperty("_port", "161");
-//        assinstanceproperty[13] = new SSInstanceProperty("_frequency", "600");
-//        assinstanceproperty[7] = new SSInstanceProperty("error-condition0", "default");
-//        assinstanceproperty[8] = new SSInstanceProperty("warning-condition0", "default");
-//        assinstanceproperty[9] = new SSInstanceProperty("good-condition0", "default");
-//        
-//		COM.dragonflow.Api.SSStringReturnValue ssstringreturnvalue2 = apimonitor.create("SNMPMonitor", "Group0", assinstanceproperty);
-//		
-//		
-		
-		
-		
-		
+		for (int i = 0; i < list.size(); i++) {
+			Map<String, String> map = list.get(i);
+			for (Entry<String, String> entry : map.entrySet()) {
+				String k = entry.getKey();
+				String v = entry.getValue();
+				assinstanceproperty[i] = new SSInstanceProperty(k, v);
+			}
+		}
+//		assinstanceproperty[0] = new SSInstanceProperty("_host",
+//				"192.168.0.248");
+//		assinstanceproperty[1] = new SSInstanceProperty("_oidIndex", "0");
+//		assinstanceproperty[2] = new SSInstanceProperty("_timeout", "5");
+//		assinstanceproperty[3] = new SSInstanceProperty("_snmpversion", "v2");
+//		assinstanceproperty[4] = new SSInstanceProperty("_retryDelay", "1");
+//		assinstanceproperty[5] = new SSInstanceProperty("_community", "dragon");
+//		assinstanceproperty[6] = new SSInstanceProperty("_frequency", "600");
+//		assinstanceproperty[7] = new SSInstanceProperty("error-condition0",
+//				"default");
+//		assinstanceproperty[8] = new SSInstanceProperty("warning-condition0",
+//				"default");
+//		assinstanceproperty[9] = new SSInstanceProperty("good-condition0",
+//				"default");
+
+		COM.dragonflow.Api.SSStringReturnValue ssstringreturnvalue2 = apimonitor
+				.create("SNMPMonitor", "Group0", assinstanceproperty);
+
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		// SSInstanceProperty[] assinstanceproperty = new
+		// SSInstanceProperty[15];
+		//
+		// assinstanceproperty[0] = new SSInstanceProperty("_nbmonitorVersion",
+		// "-1");
+		// assinstanceproperty[1] = new SSInstanceProperty("_nbmonitorVersion",
+		// "-1");
+		// assinstanceproperty[2] = new SSInstanceProperty("_community",
+		// "dragon");
+		// assinstanceproperty[3] = new SSInstanceProperty("_community",
+		// "dragon");
+		// assinstanceproperty[4] = new SSInstanceProperty("_maxRTDataWindow",
+		// "24");
+		// assinstanceproperty[5] = new SSInstanceProperty("_timeout", "5");
+		// assinstanceproperty[6] = new SSInstanceProperty("_timeout", "5");
+		// assinstanceproperty[10] = new SSInstanceProperty("_retries", "1");
+		// assinstanceproperty[11] = new SSInstanceProperty("_retries", "1");
+		// assinstanceproperty[12] = new SSInstanceProperty("_port", "161");
+		// assinstanceproperty[13] = new SSInstanceProperty("_port", "161");
+		// assinstanceproperty[13] = new SSInstanceProperty("_frequency",
+		// "600");
+		// assinstanceproperty[7] = new SSInstanceProperty("error-condition0",
+		// "default");
+		// assinstanceproperty[8] = new SSInstanceProperty("warning-condition0",
+		// "default");
+		// assinstanceproperty[9] = new SSInstanceProperty("good-condition0",
+		// "default");
+		//
+		// COM.dragonflow.Api.SSStringReturnValue ssstringreturnvalue2 =
+		// apimonitor.create("SNMPMonitor", "Group0", assinstanceproperty);
+		//
+		//
+
 	}
 }
