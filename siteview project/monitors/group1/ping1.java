3490ecff0e334e5c89285c94ec0e0681785500fc
John <zejun.zhang@gmail.com>
2012-04-27 09:46:38 +0800

    add siteview project

---
+++ siteview project/monitors/group1/ping1.java
@@ -0,0 +1,14 @@
+package group1;
+
+public interface ping1 {
+	public static final String _class="PingMonitor";
+	public static final String 		_id="3";
+	public static final String 	_encoding="GBK";
+	public static final String 		_packetSize="32";
+	public static final String 		_frequency="600";
+	public static final String 		_dependsCondition="good";
+	public static final String 		_timeout="5000";
+	public static final String 		_internalId="83964295";
+	public static final String 		_name="Ping: www.baidu.com";
+	public static final String 		_hostname="www.baidu.com";
+}
