b4e3f664177c901b22e0420dc212b37429d23e2d
wang-zhong-ping <wang-zhong-ping@163.com>
2012-05-18 18:38:11 +0800

    add zk8.13 call method

---
+++ ecc/src/com/siteview/jsvapi/Test.java
@@ -0,0 +1,16 @@
+package com.siteview.jsvapi;
+
+public class Test {
+	public static void nativeDll(){
+		System.loadLibrary("swprv");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		nativeDll();
+	}
+
+}
