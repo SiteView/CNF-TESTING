8943057f5ca818794b273304102f8a829e95cd6c
wang-zhong-ping <wang-zhong-ping@163.com>
2012-06-12 13:48:13 +0800

    Add tools.

---
+++ ecc/src/SiteView/ecc/tools/Config.java
@@ -0,0 +1,35 @@
+package SiteView.ecc.tools;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+/**
+ * @author zhongping.wang
+ * <p>
+ * 读取配置文件properties文件
+ * </p>
+ */
+public class Config {
+	public static final Properties prop = new Properties();
+
+	/**
+	 * 获取参数
+	 * 
+	 * @return
+	 */
+	public static String getReturnStr(String filePath, String parm) {
+		try {
+			FileInputStream fis = new FileInputStream(new File(filePath));
+			prop.load(fis);
+		}catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return prop.getProperty(parm);
+	}
+}
---
+++ ecc/src/SiteView/ecc/tools/FileTools.java
@@ -0,0 +1,48 @@
+package SiteView.ecc.tools;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+
+import SiteView.ecc.Activator;
+
+import system.Data.DataException;
+
+/**
+ * 文件操作类
+ * 
+ * @author Administrator
+ * 
+ */
+public class FileTools {
+	public static final String PLUGIN_ID = "ecc";
+
+	// 获取文件路径
+	public static String getRealPath(String fileName) {
+		URL urlentry;
+		String strEntry;
+		try {
+			Bundle bundle = Platform.getBundle(PLUGIN_ID);
+			if (bundle == null)
+				throw new DataException("请检查文件的路径", new NullPointerException());
+			// get path URL
+			urlentry = bundle.getEntry(fileName);
+			if (urlentry == null)
+				throw new DataException("请检查文件的路径", new NullPointerException());
+			strEntry = FileLocator.toFileURL(urlentry).getPath();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new DataException("请检查文件的路径", e);
+		}
+		return strEntry;
+	}
+
+	// 获取插件路径
+	public static String getPluginPath() {
+		return Activator.getDefault().getStateLocation().makeAbsolute()
+				.toFile().getAbsolutePath();
+	}
+}
